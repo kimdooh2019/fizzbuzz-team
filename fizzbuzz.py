8142a1298bbc73beb1905394e1e4c36b808ef451
min-jyo <minjyo95@gmail.com>
2020-01-31 14:11:36 +0900

    3,5,15배수로 각 다른 출력

---
+++ fizzbuzz.py
@@ -0,0 +1,7 @@
+for i in range(1, 101):
+    if i % 3 == 0:
+        print("3의 배수입니다 : fizz")
+    if i % 5 == 0:
+        print("5의 배수입니다 : buzz")
+    if i % 15 == 0:
+        print("15의 배수입니다 : fizzbuzz")
