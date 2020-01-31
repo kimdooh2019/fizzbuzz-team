b5828935e2f09b0081ed2af8dad05f9fa9428491
hyeonggeun2 <dhgudrms12@gmail.com>
2020-01-31 15:13:36 +0900

    fizzbuzz makes to lambda and one if

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,9 @@
+result = list(map(lambda x: "fizzbuzz" if x%15 == 0 else "fizz" if x%5 == 0 else "buzz" if x%3 ==0 else x, [i for i in range(1, 101)]))
+
+print(result)
 
 for i in range(1, 101):
-    if i % 3 == 0:
-        print("3의 배수입니다 : fizz")
-    if i % 5 == 0:
-        print("5의 배수입니다 : buzz")
-    if i % 15 == 0:
-        print("15의 배수입니다 : fizzbuzz")
-
-# result = ["fizzbuzz" if i%15==0 else 
-#     "fizz" if i%3==0 else 
-#     "buzz" if i%5==0 else 
-#     i for i in range(1,100+1)
-#     ]
-
-# print(result)
-
+    if (i%3 == 0 or i%5 == 0):
+        print("fizz" * (i%3 == 0) + "buzz" * (i%5 == 0))
+    else:
+        print(i)
